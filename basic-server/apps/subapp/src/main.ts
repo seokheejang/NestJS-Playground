361073204f5461d25328b5eb7aa2a6bf4b0ef21a
seokheejang <shjang@dkargo.io>
2023-12-26 11:45:14 +0900

    Revert "Test nest-cli sub-app"
    This reverts commit ee0a2cc2d243bcdd8842fda5169d4438e57a7584.

--- basic-server/apps/subapp/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SubappModule } from './subapp.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(SubappModule);
-  await app.listen(3001);
-}
-bootstrap();
--- basic-server/apps/subapp/src/subapp.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class SubappService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
